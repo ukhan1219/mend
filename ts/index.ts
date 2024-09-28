a4e9dfe8624495a35d6d99aea91556112b3a5db5
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-09-28 12:55:36 -0400

    php

--- ts/index.ts
+++ ts/index.ts
@@ -159,7 +159,7 @@ function saveCookie() {
     let minutes = 20;
     let date = new Date();
     date.setTime(date.getTime() + (minutes * 60 * 1000));
-    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userID=" + userID + ";expires=" + date.toGMTString();
+    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userID=" + userID + ";expires=" + date.toUTCString();
 }
 
 function readCookie() {
