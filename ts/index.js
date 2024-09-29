86bc9fd7ad4358f56420d93bf649caaa02f887f4
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-09-28 23:34:09 -0400

    killl

--- ts/index.js
+++ ts/index.js
@@ -174,7 +174,7 @@ function doLogin() {
                 saveCookie();
 
 
-                window.location.href = "pages/dashboard.html";
+                window.location.href = "../pages/dashboard.html";
             }
         };
         xhr.send(jsonPayload);
