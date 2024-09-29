b9230ebb988ac39d14387d57bd859cca9a32883e
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-09-28 21:59:30 -0400

    username

--- api/register.php
+++ api/register.php
@@ -9,7 +9,7 @@ $username = $inData['username'];
 $password = $inData['password'];
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-$conn = new mysqli("localhost", "API", "APIPASSWORD", "mend");
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "mend");
 
 if ($conn->connect_error){
     returnWithError($conn->connect_error);
